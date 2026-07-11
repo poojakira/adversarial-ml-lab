44aac6408cb12bb409c2fb42cb530e59264b602c
poojakira <poojakira@users.noreply.github.com>
2026-07-11 03:01:41 +0000

    feat: adversarial training loop with PGD-7 inner attack

---
+++ src/adv_lab/defenses/__init__.py
@@ -0,0 +1,5 @@
+"""Defenses. Currently the one empirical defense that has survived: adversarial training."""
+
+from adv_lab.defenses.adversarial_training import AdversarialTrainer
+
+__all__ = ["AdversarialTrainer"]
